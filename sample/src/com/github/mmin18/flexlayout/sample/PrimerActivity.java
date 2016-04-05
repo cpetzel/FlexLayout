94fd870862d840e418e220566d29a3041a03352d
Craig Petzel <craig@sendbridge.com>
2016-04-05 15:00:28 -0700

    adding layout for primer

---
+++ sample/src/com/github/mmin18/flexlayout/sample/PrimerActivity.java
@@ -0,0 +1,27 @@
+package com.github.mmin18.flexlayout.sample;
+
+import android.app.Activity;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.regex.Pattern;
+
+/**
+ * Created by craig on 4/5/16.
+ */
+public class PrimerActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.primer_layout);
+
+    }
+
+
+}
